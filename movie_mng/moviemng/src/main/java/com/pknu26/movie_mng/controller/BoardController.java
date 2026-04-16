a738803eae1e31c7b15163d7f6e445b793b44240
spartajaden <jaden758@naver.com>
2026-04-16 21:19:13 +0900

    미니 프로젝트 저장!!

--- movie_mng/moviemng/src/main/java/com/pknu26/movie_mng/controller/BoardController.java
+++ movie_mng/moviemng/src/main/java/com/pknu26/movie_mng/controller/BoardController.java
@@ -2,11 +2,11 @@ package com.pknu26.movie_mng.controller;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
--- movie_mng/moviemng/src/main/java/com/pknu26/movie_mng/service/ReplyService.java
+++ movie_mng/moviemng/src/main/java/com/pknu26/movie_mng/service/ReplyService.java
@@ -1,7 +1,10 @@
 package com.pknu26.movie_mng.service;
 
+import org.springframework.stereotype.Service;
+
 import com.pknu26.movie_mng.entity.Board;
 
+@Service
 public class ReplyService {
 
     public void setReply(Board board, Object content) {
