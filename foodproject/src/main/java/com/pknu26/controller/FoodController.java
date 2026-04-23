e37333975a2b6f578035c9ced7ae6c375dcf7025
spartajaden <jaden758@naver.com>
2026-04-23 22:06:53 +0900

    커밋

---
+++ foodproject/src/main/java/com/pknu26/controller/FoodController.java
@@ -0,0 +1,5 @@
+package com.pknu26.controller;
+
+public class FoodController {
+
+}
---
+++ foodproject/src/test/java/com/pknu26/foodproject/FoodprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pknu26.foodproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
