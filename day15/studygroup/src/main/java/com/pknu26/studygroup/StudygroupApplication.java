8ce89aa79299a291848e3805253f8c082dfeec73
spartajaden <jaden758@naver.com>
2026-04-23 17:30:40 +0900

    15일차 커밋

--- day15/studygroup/src/main/java/com/pknu26/studygroup/StudygroupApplication.java
+++ day15/studygroup/src/main/java/com/pknu26/studygroup/StudygroupApplication.java
@@ -4,21 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-import com.pknu26.studygroup.config.FileProperties;
-
 @SpringBootApplication
 public class StudygroupApplication {
+
 
 	public static void main(String[] args) {
 		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 		String encoded = encoder.encode("p@ssw0rd!");
 		System.out.println("password - " + encoded);
-
+		
 		SpringApplication.run(StudygroupApplication.class, args);
-		
-		FileProperties properties = new FileProperties();
-		
-		System.out.println("uploadDir - " + uploadDir);
 	}
 
 }
